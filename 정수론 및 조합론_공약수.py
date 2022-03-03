d4434e837209a8f3d2f1ff5f0af6f0618cd559e9
hamrabong <anne37053@gmail.com>
2022-03-04 00:37:56 +0900

    Add files via upload

---
+++ 정수론 및 조합론_공약수.py
@@ -0,0 +1,20 @@
+def gcd(a, b):
+    while b:
+        mod = b
+        b = a%b
+        a = mod
+    return a
+
+g, l = map(int, input().split())
+div = l // g
+a, b = 1, div
+for i in range(1, div//2+1):
+    if div % i == 0:
+        c = i
+        d = div//i
+        if gcd(c, d) != 1:
+            continue
+        if a+b > c+d:
+            a = c
+            b = d
+print(a*g, b*g)
